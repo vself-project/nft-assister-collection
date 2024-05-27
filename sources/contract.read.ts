dd7f25e4eaffc02c5351c069faa9c70ef27a9e22
SergantChe <sergei.alex.kozlov@gmail.com>
2024-05-27 19:43:13 +0300

    update read script

--- sources/contract.read.ts
+++ sources/contract.read.ts
@@ -7,20 +7,20 @@ import { NftCollection } from "./output/sample_NftCollection";
 
 (async () => {
     const client = new TonClient4({
-        // endpoint: "https://mainnet-v4.tonhubapi.com", // 🔴 Main-net API endpoint
-        endpoint: "https://sandbox-v4.tonhubapi.com", // 🔴 Test-net API endpoint
+        endpoint: "https://mainnet-v4.tonhubapi.com", // 🔴 Main-net API endpoint
+        //endpoint: "https://sandbox-v4.tonhubapi.com", // 🔴 Test-net API endpoint
     });
 
     // Parameters
-    let collection_address = Address.parse("EQATBrkvgrggXJjc9UcRBNI1IxWm25ZbBfkFeaZxvAgVapYG");
+    const collection_address = Address.parse("EQDZv2Glu_Gmq30lQ9dCXHQN7Ht5NhGkvTJ0DrRvSXmx2kh0"); // https://getgems.io/collection/EQDZv2Glu_Gmq30lQ9dCXHQN7Ht5NhGkvTJ0DrRvSXmx2kh0
+    const collection_contract = await NftCollection.fromAddress(collection_address);
+    let client_open = client.open(collection_contract);
 
-    let contract_address = await NftCollection.fromAddress(collection_address);
-    let client_open = client.open(contract_address);
-
-    const nft_index = 0n;
+    const nft_index = 1n;
     let address_by_index = await client_open.getGetNftAddressByIndex(nft_index);
     printHeader("sampleNFT_Contract");
-    // printAddress(collection_address
+    //printAddress(collection_address);
     // printHeader("1234");
     console.log("NFT ID[" + nft_index + "]: " + address_by_index);
+    console.log(`NFT Explorer: https://explorer.tonnft.tools/nft/${address_by_index}`);
 })();
